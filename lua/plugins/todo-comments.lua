b3ff10d2d330919979a72f02f6eacb3284924f99
iamsurjog <iamsurjog@gmail.com>
2026-03-21 15:38:59 +0530

    feat(TODO): todo comments keybind

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,4 +1,8 @@
-return{ 
-    "folke/todo-comments.nvim", 
+return {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    keys = {
+        { "<leader>tt", function() require("snacks").picker.todo_comments() end, desc = "Open TODO buffer" }
+    },
     opts = {}
 }
