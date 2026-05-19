92024e97cd16442e059061aade48aa444cb2acf9
iamsurjog <iamsurjog@gmail.com>
2026-05-19 11:09:26 +0530

    clear

--- lua/plugins/todo-comments.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "folke/todo-comments.nvim",
-    lazy = false,
-    keys = {
-        { "<leader>tt", function() require("snacks").picker.todo_comments() end, desc = "Open TODO buffer" }
-    },
-    opts = {}
-}
